d672867ffd033754cab6e4028073aa9074e6026c
Denis Lehmann <denis@opaque.tech>
2021-11-01 19:55:01 +0100

    change flake output

--- flake.nix
+++ flake.nix
@@ -1,28 +1,22 @@
 {
   description = "socialize web page";
-
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
   outputs = { self, nixpkgs, flake-utils }:
-
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-
-          # Package
           packages.socialize =
             pkgs.stdenv.mkDerivation {
               name = "socialize";
               src = self;
               installPhase = ''
-                install -m 444 -D index.html $out/var/www/socialize/index.html
+                install -m 444 -D index.html $out/index.html
               '';
             };
           defaultPackage = self.packages.${system}.socialize;
         }
-
       );
 }
